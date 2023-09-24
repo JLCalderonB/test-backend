653aece4c79af61de22f16f0a7bebc70083a961b
Juan Luis Calderón <jl.calderon.b@gmail.com>
2023-09-24 16:58:59 -0300

    Corrige modelo Intentologin

--- models/Intentologin.js
+++ models/Intentologin.js
@@ -5,8 +5,8 @@ const sequelize = require('../db-connection');
 
 const Intentologin = sequelize.define('Intentologin', {
     intento_login_id: {
-        type: DataTypes.INTEGER,
-        autoincrement: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
         allowNull: false
     },
