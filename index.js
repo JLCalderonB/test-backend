7bbabce7508d2a2c31b4f53c9476e755f06a6b1c
Juan Luis Calderón <jl.calderon.b@gmail.com>
2023-09-25 01:49:41 -0300

    Primer Post Perfil

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ sequelize
         });
 })();
 // Define tus modelos y relaciones Sequelize aquí
+const Perfil = sequelize.define('Perfil', {
+    perfil_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+    },
+    perfil_desc: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    }
+},
+    {
+        freezeTableName: true,
+        Timestamps: false
+    });
 
 // Rutas de tu aplicación Express
 app.use((req, res, next) => {
