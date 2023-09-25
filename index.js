942dc6fca86da88e5b3e02db6350d5b03749aee3
Juan Luis Calderón <jl.calderon.b@gmail.com>
2023-09-24 23:28:01 -0300

    CRUD Perfil

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const sequelize = require('./db-connection');
 const CreatDB = require('./models/CreaDB'); // Ajusta la ruta según tu estructura de carpetas
 
 const app = express();
+app.use(bodyParser.json());
+
 
 // Verifica si la conexión a la base de datos es exitosa
 sequelize
@@ -14,7 +18,7 @@ sequelize
         console.error('Error al conectar a la base de datos:', error);
     });
 
-sequelize.sync({ force: true })
+sequelize.sync({ alter: true })
     .then(() => {
         console.log('Modelos sincronizados con la base de datos');
     })
@@ -24,8 +28,80 @@ sequelize.sync({ force: true })
 // Define tus modelos y relaciones Sequelize aquí
 
 // Rutas de tu aplicación Express
-
-app.listen(3000, () => {
-    console.log('Servidor Express TFM escuchando en el puerto 3000');
+// Create a new Perfil
+app.post('/perfil', async (req, res) => {
+    try {
+        const { perfil_id, perfil_desc } = req.body;
+        const Perfil = await Perfil.create({ perfil_id, perfil_desc });
+        res.status(201).json(Perfil);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+
+// Get all perfiles
+app.get('/perfiles', async (req, res) => {
+    try {
+        const Perfil = await Perfil.findAll();
+        res.status(200).json(Perfil);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Get a single perfil by ID
+app.get('/perfiles/:id', async (req, res) => {
+    try {
+        const Perfil = await Perfil.findByPk(req.params.id);
+        if (!Perfil) {
+            res.status(404).json({ error: 'Perfil not found' });
+        } else {
+            res.status(200).json(book);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Update a perfil by ID
+app.put('/perfiles/:id', async (req, res) => {
+    try {
+        const { perfil_id, perfil_desc } = req.body;
+        const Perfil = await Perfil.findByPk(req.params.id);
+        if (!Perfil) {
+            res.status(404).json({ error: 'Perfil not found' });
+        } else {
+            Perfil.perfil_id = perfil_id || Perfil.perfil_id;
+            Perfil.perfil_desc = perfil_desc || Perfil.perfil_desc;
+            await Perfil.save();
+            res.status(200).json(book);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Delete a perfil by ID
+app.delete('/perfil/:id', async (req, res) => {
+    try {
+        const Perfil = await Perfil.findByPk(req.params.id);
+        if (!Perfil) {
+            res.status(404).json({ error: 'Perfil not found' });
+        } else {
+            await Perfil.destroy();
+            res.status(204).send();
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`TFM Server is running on port ${PORT}`);
+});
+
+//app.listen(3000, () => {
+//    console.log('Servidor Express TFM escuchando en el puerto 3000');
+//});
 
